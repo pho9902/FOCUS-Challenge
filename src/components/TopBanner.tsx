ba8692bd87a47776469ab32f02011ee837f826c6
unknown <pho9902@naver.com>
2022-01-10 10:18:11 +0900

    topBanner 컴포넌트 생성, css 일부 적용

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -1,8 +1,14 @@
+import styles from "styles/TopBanner.module.scss";
+
 export default function TopBanner() {
   return (
-    <div>
-      <div>지금 가입하고 원하는 클래스 1개 무료 체험하기</div>
-      <button>X</button>
+    <div className={styles.wrap}>
+      <div className={styles.container}>
+        <div className={styles.span}>
+          지금 가입하고 원하는 클래스 1개 무료 체험하기
+        </div>
+        <span className={styles.closeBtn}>X</span>
+      </div>
     </div>
   );
 }
