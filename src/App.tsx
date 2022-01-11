d5d13573a8e204c47f31cbb6a9d452b7ee0c5f6a
unknown <pho9902@naver.com>
2022-01-11 15:23:53 +0900

    헤더 하단부분 디자인, 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import TopBanner from "./components/TopBanner";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function App() {
   const [isOpen, setIsOpen] = useState<boolean>(true);
