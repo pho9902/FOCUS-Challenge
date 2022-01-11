9757153386084df85a95e4f6fec02cb228e5a9dc
unknown <pho9902@naver.com>
2022-01-11 15:28:10 +0900

    해야 할 일 app.tsx 파일에 정리

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,14 @@ export default function App() {
     <div>
       {isOpen && <TopBanner setIsOpen={setIsOpen} />}
       <Header />
+      {/* 캐러셀 */}
+      {/* 캐러셀밑 배너 */}
+      {/* 오늘의특가 섹션 */}
+      {/* MD 추천 클래스 */}
+      {/* 진행중인 인기 이벤트 */}
+      {/* 오픈 예정 클래스 */}
+      {/* 반응형까지 다하면 캐러셀 autoplay, 썸네일 이미지에 animation */}
+      {/* 위에꺼 다하면 카테고리, 검색 툴팁 */}
     </div>
   );
 }
